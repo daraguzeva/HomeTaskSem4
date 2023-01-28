6b128ab39643baa2cba233cbc2dbca5db1f41dba
Daria <lara-lorn@yandex.ru>
2023-01-28 13:58:29 +0400

    save task

--- Task029/Program.cs
+++ Task029/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+int[] array = new int[8];
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(0, 101);
+    Console.Write($"{array[i]} ");
+}
